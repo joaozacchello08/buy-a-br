837c77021aebf70bbcef7d6bf4459748a86b4ef5
João Carlos Castro Zacchello <joaozacchello@xongs-macbook-air.local>
2025-08-24 17:36:15 -0300

    added a react version

---
+++ react-version/src/components/brainrot-card/BrainrotCard.jsx
@@ -0,0 +1,10 @@
+import './BrainrotCard.css'
+
+export default function BrainrotCard(
+                        BrainrotName = "",
+                        BrainrotImage = "",
+                        BrainrotRarity = [],
+                        BrainrotPrice = 0
+                    ) {
+
+}
