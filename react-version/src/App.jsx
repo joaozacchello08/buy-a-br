cf6bb378896bcc38ca2916ac8b2c0583bbd547ce
Joao Carlos <xonguinhos@gmail.com>
2025-08-24 18:54:24 -0300

    im gonna kill css

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -1,7 +1,50 @@
-import './App.css'
+import BrainrotCard from "./components/brainrot-card/BrainrotCard"
+import "./App.css"
 
 function App() {
-  return <h1>A</h1>
+  const brainrots = [
+    {
+      name: "Brainrot 1",
+      image: "https://static.wikia.nocookie.net/character-stats-and-profiles/images/6/69/Lirili_Larila_v2.png",
+      alt: "Card 1",
+      rarity: [["Galaxy", "#00f"]],
+      price: 100,
+      mps: 5
+    },
+    {
+      name: "Tralalero Tralala",
+      image: "https://images.cults3d.com/VSyY3jKKi3zAG_246OpS54J3_8s=/516x516/filters:no_upscale()/https://fbi.cults3d.com/uploaders/26588735/illustration-file/49a806c1-a349-4051-90db-a2e1a6e80bf9/Tralalero-Tralala.png",
+      alt: "Card 2",
+      rarity: [["Secret", "#0f0"]],
+      price: 200,
+      mps: 10
+    },
+    {
+      name: "Brainrot 3",
+      image: "https://via.placeholder.com/200x300",
+      alt: "Card 3",
+      rarity: [["Rare", "#f00"]],
+      price: 300,
+      mps: 20
+    }
+  ]
+
+  return (
+    <div className="content">
+      {brainrots.map((b, i) => (
+        <BrainrotCard
+          key={i}
+          BrainrotName={b.name}
+          BrainrotImage={b.image}
+          AltText={b.alt}
+          BrainrotRarity={b.rarity}
+          BrainrotPrice={b.price}
+          BrainrotMoneyPerSecond={b.mps}
+          style={{ animationDelay: `${i * 3}s` }}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App
--- react-version/src/components/brainrot-card/BrainrotCard.jsx
+++ react-version/src/components/brainrot-card/BrainrotCard.jsx
@@ -1,10 +1,23 @@
-import './BrainrotCard.css'
-
-export default function BrainrotCard(
-                        BrainrotName = "",
-                        BrainrotImage = "",
-                        BrainrotRarity = [],
-                        BrainrotPrice = 0
-                    ) {
-
+export default function BrainrotCard({
+  BrainrotName = "",
+  BrainrotImage = "",
+  AltText = "",
+  BrainrotRarity = [["Rarity", "#COLOR"]],
+  BrainrotPrice = 0,
+  BrainrotMoneyPerSecond = 0,
+  style = {}
+}) {
+  return (
+    <div className="brainrot-card" style={style}>
+      {BrainrotRarity.map(([name, color], index) => (
+        <div key={index} style={{ color }}>
+          {name}
+        </div>
+      ))}
+      <h3>{BrainrotName}</h3>
+      <img src={BrainrotImage} alt={AltText} />
+      <span>${BrainrotMoneyPerSecond}/s</span>
+      <span>${BrainrotPrice}</span>
+    </div>
+  )
 }
